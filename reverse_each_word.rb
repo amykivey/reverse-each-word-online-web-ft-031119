ceb99a8596e4f82548b6beec7c3407cc92f94a1b
kyle smith <hirekyle@someotherkyle.com>
2021-02-09 01:10:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,8 @@ end
 def reverse_each_word(phrase)
   array = phrase.split
   array.collect do |word|
-    word.reverse
-    array.join(' ')
-  end
+    new = [] 
+    new << word.reverse
+  end]
+  new.join(' ')
 end
