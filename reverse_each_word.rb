7c5d1614034eee91c3318ea0011a157b5be6c133
kyle smith <hirekyle@someotherkyle.com>
2021-02-09 01:21:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
-def reverse_each_word(phrase)
-  array = phrase.split
-  new = []
-  array.each do |word|
-    new << word.reverse
-  end
-  new = new.join(' ')
-  new
-  
-end
+
 
 def reverse_each_word(phrase)
   array = phrase.split
